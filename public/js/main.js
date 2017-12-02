b74d7af13acda6011dcd53c44427817cd17c3cda
Thea Kreis <tkreis1@students.towson.edu>
2017-12-02 14:52:12 -0500

    Update individdash image gallery to show/hide images.

--- public/js/main.js
+++ public/js/main.js
@@ -45,11 +45,53 @@ $(document).ready(function() {
         $(this).attr("href", _href +logID );
     });
 
-    //Image carousel
-    // $(document).ready(function(){
-    //     $('.img-carousel').slick({
-    //         arrows: true
-    //     });
+
+
+    $('.individgallery li:lt(3)').show();
+    $('#less').hide();
+    var items =  6;
+    var minshow = 3;
+    var shown =  3;
+    var nowshowing = 0;
+    $('#more').click(function () {
+        shown = $('.individgallery li:visible').length +3;
+        nowshowing = shown;
+        if(shown < items) {
+            $('.individgallery li:lt('+shown+')').show();
+            $('#more').show();
+            // $('#less').removeClass('hidden');
+            $('#less').show();
+            return false;
+        }
+        else {
+            $('.individgallery li:lt('+items+')').show();
+            // $('#less').removeClass('hidden');
+            $('#less').show();
+            $('#more').hide();  
+            return false; 
+
+        }
+    });
+    $('#less').click(function () {
+        $('.individgallery li').not(':lt(3)').hide();
+        nowshowing = shown -3;
+        $('#more').show();        
+        if(nowshowing == minshow){
+            $('#less').hide();
+        }
+        return false;         
+    });
+
+
+    // $('#pagination-individ').twbsPagination({
+    //     totalPages: 2,
+    //     visiblePages: 2,
+    //     next: 'Next',
+    //     prev: 'Prev',
+    //     onPageClick: function (event, page) {
+    //         //fetch content and render here
+    //         $('#page-content').text('Page ' + page) + ' content here';
+    //     }
     // });
 
     //Group dash forum posts
